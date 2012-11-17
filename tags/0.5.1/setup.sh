9d6e93a18bc27c6c2857f469b3491810f1ad2280
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2012-11-17 18:45:11 +0000

    tagging 0.5.1

---
+++ tags/0.5.1/setup.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+SEARCH="python python3"
+FOUND=""
+INSTALLED=""
+
+echo "Installing WebIOPi...\n"
+
+cd python
+
+for python in $SEARCH; do
+	program=`which $python`
+	if [ -x $program ]; then
+		FOUND="$FOUND $python"
+		version=`$python -V 2>&1`
+		include=`$python -c "import distutils.sysconfig; print(distutils.sysconfig.get_python_inc())"`
+		echo "Found $version... " 
+		if [ -f "$include/Python.h" ]; then
+			$python setup.py install || (echo "Build for $version failed\n" && continue) 
+			echo "WebIOPi installed for $version\n"
+			INSTALLED="$INSTALLED $python"
+	    else
+			echo "Cannot install for $version : missing development headers\n"
+		fi
+	fi
+done
+
+cd ..
+
+if [ -z "$INSTALLED" ]; then
+	if [ -z "$FOUND" ]; then
+		echo "ERROR: WebIOPi cannot be installed - neither python or python3 found"
+	else
+		echo "ERROR: WebIOPi cannot be installed - please install python development package first"
+	fi
+	exit
+fi
+
+for python in $INSTALLED; do
+	echo $python > /dev/null
+done 
+
+echo -n "Copying resources... " 
+mkdir /usr/share/webiopi 2>/dev/null 1>/dev/null
+cp -rf htdocs /usr/share/webiopi
+cp -rf python/webiopi.py.init /etc/init.d/webiopi
+chmod 0755 /etc/init.d/webiopi
+sed -i "s/python/$python/g" /etc/init.d/webiopi
+echo "OK\n"
+echo "WebIOPi successfully installed"
+
+for python in $INSTALLED; do
+	version=`$python -V 2>&1`
+	echo "* You can use it with $version\t: sudo $python -m webiopi"
+done
