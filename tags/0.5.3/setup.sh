d6c0ecbbb46334189f6b7ca39a54a322ffff6f77
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2013-01-08 19:25:14 +0000

    Tagged 0.5.3

---
+++ tags/0.5.3/setup.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+# WebIOPi setup script
+
+SEARCH="python python3"
+FOUND=""
+INSTALLED=""
+
+echo "Installing WebIOPi...\n"
+
+# Install Python library
+cd python
+
+# Look up for installed python
+for python in $SEARCH; do
+	program="/usr/bin/$python"
+	if [ -x $program ]; then
+		FOUND="$FOUND $python"
+		version=`$python -V 2>&1`
+		include=`$python -c "import distutils.sysconfig; print(distutils.sysconfig.get_python_inc())"`
+		echo "Found $version... "
+
+		# Install required dev header if not present
+		if [ ! -f "$include/Python.h" ]; then
+			echo "Trying to install $python-dev using apt-get"
+			apt-get install -y "$python-dev"
+		fi
+
+		# Try to compile and install for the current python
+		if [ -f "$include/Python.h" ]; then
+			echo "Trying to install WebIOPi for $version"
+			$python setup.py install
+			if [ "$?" -ne "0" ]; then
+				# Sub setup error, continue with next python
+				echo "Build for $version failed\n"
+				continue
+			fi
+			echo "WebIOPi installed for $version\n"
+			INSTALLED="$INSTALLED $python"
+		else
+			echo "Cannot install for $version : missing development headers\n"
+		fi
+	fi
+done
+
+# Go back to the root folder
+cd ..
+
+# Ensure WebIOPi is installed to continue
+if [ -z "$INSTALLED" ]; then
+	if [ -z "$FOUND" ]; then
+		echo "ERROR: WebIOPi cannot be installed - neither python or python3 found"
+		exit 1
+	else
+		echo "ERROR: WebIOPi cannot be installed - please check errors above"
+		exit 2
+	fi
+fi
+
+# Select greater python version
+for python in $INSTALLED; do
+	echo $python > /dev/null
+done 
+
+# Copy mandatory and optional resource files
+
+# Mandatory update
+echo "Copying resources..."
+mkdir /usr/share/webiopi 2>/dev/null 1>/dev/null
+cp -rf htdocs /usr/share/webiopi
+cp -rf python/webiopi-passwd.py /usr/bin/webiopi-passwd
+sed -i "s/python/$python/g" /usr/bin/webiopi-passwd
+
+# Add passwd file if it does not exist
+if [ ! -f "/etc/webiopi/passwd" ]; then
+	mkdir /etc/webiopi 2>/dev/null 1>/dev/null
+	cp python/passwd /etc/webiopi/passwd
+fi
+echo
+
+# Add service/daemon script if it does not exist
+if [ ! -f "/etc/init.d/webiopi" ]; then
+	echo "Setting up startup script..."
+	cp -rf python/webiopi.py.init /etc/init.d/webiopi
+	chmod 0755 /etc/init.d/webiopi
+	sed -i "s/python/$python/g" /etc/init.d/webiopi
+	echo
+fi
+
+# Display WebIOPi usages
+echo "WebIOPi successfully installed"
+for python in $INSTALLED; do
+        echo "* To start WebIOPi with $python\t: sudo $python -m webiopi"
+done
+echo
+echo "* To start WebIOPi at boot\t: sudo update-rc.d webiopi defaults"
+echo "* To start WebIOPi service\t: sudo /etc/init.d/webiopi start"
+echo
+echo "* Look in `pwd`/examples for Python library usage examples"
+echo
